82244e350f1b634c59104ac3d9ac0020f2c56060
Iradiel <iradiel90@gmail.com>
2012-12-20 21:03:05 +0000

    making up more crazy stories

--- vdl-themes/default/inbox.php
+++ vdl-themes/default/inbox.php
@@ -16,7 +16,67 @@ You should have received a copy of the GNU General Public License
 along with Foobar.  If not, see <http://www.gnu.org/licenses/>.*/
 ?>
 
+<!--
+<script>
+	
+	var autocomplete = $('#remitte678').typeahead()
+		.on('keyup', function(ev){
+ 
+            ev.stopPropagation();
+            ev.preventDefault();
+ 
+            //filter out up/down, tab, enter, and escape keys
+            if( $.inArray(ev.keyCode,[40,38,9,13,27]) === -1 ){
+ 
+                var self = $(this);
+                
+                //set typeahead source to empty
+                self.data('typeahead').source = [];
+ 
+                //active used so we aren't triggering duplicate keyup events
+                if( !self.data('active') && self.val().length > 0){
+ 
+                    self.data('active', true);
+ 
+                    //Do data request. Insert your own API logic here.
+                    $.getJSON('< ?php echo BASEDIR;?>/vdl-include/user_autocomplete.php', {
+                        q: $(this).val()
+                    },  function(data) {
+ 
+                        //set this to true when your callback executes
+                        self.data('active',true);
+ 
+                        //Filter out your own parameters. Populate them into an array, since this is what typeahead's source requires
+                        var arr = [],
+                            i=data.results.length;
+                        while(i--){
+                            arr[i] = data.results[i].text
+                        }
+ 
+                        //set your results into the typehead's source 
+                        self.data('typeahead').source = arr;
+ 
+                        //trigger keyup on the typeahead to make it search
+                        self.trigger('keyup');
+ 
+                        //All done, set to false to prepare for the next remote query.
+                        self.data('active', false);
+ 
+                    });
+ 
+                }
+            }
+        })
+	
+</script>
 
+
+<script>
+	$('#remitte678').typeahead({
+		source: ['jose','luis','andres','pedro']
+	});
+</script>
+-->
 
 <div align="right">
 	<a href="#myModal" role="button" data-toggle="modal" class="btn btn-primary btn-large">Nuevo mensaje</a>
