224d440fe391dc09e4b390008b00615f5a920a7c
Iradiel <iradiel90@gmail.com>
2013-01-09 18:22:00 +0000

    search in progress

--- vdl-include/search.php
+++ vdl-include/search.php
@@ -1,13 +1,7 @@
 <?php
 	//AQUI lo de llamar al core y too el jaleo
 	//por ultimo llamar al header index.php y eso (con ?action=search
-	session_start();
-	include("vdl-core/core_main.class.php");
-	include("vdl-core/core_user.class.php");
 	$c_user = new CORE_USER();
-	$found_users = $c_user->search_users("tes");
-	for ($i = 0; $i < count($found_users); $i++){
-		print_r($found_users[$i]);
-	}
-	header("Location: /Vidali/search/tes");
+	$found_users = $c_user->search_users($_POST['search']);
+	header("Location:".$_SERVER['HTTP_REFERER']."search/");
 ?>
--- vdl-themes/default/search.php
+++ vdl-themes/default/search.php
@@ -1,4 +1,20 @@
 <h1>Ola ke dise</h1>
 <?php
 	//aqui encajar todo lo visual, y comprueben como se envian las cosas
+	$c_user = new CORE_USER();
+	$found_users = $c_user->search_users("tes");
+	print_r($_POST);
 ?>
+
+<div>
+	<ul>
+		<?php
+			foreach ($found_users as $found){
+				echo '<li>';
+				echo '<img src="'.BASEDIR."/vdl-files/".$found[1].'.jpg" width="60" height="60" >';
+				echo $found[0];
+				echo '</li>';
+			}
+		?>
+	</ul>
+</div>
