ca5fc82883eeabdbca30fe763f09c2ba752c5e58
Jose <joseiherq@gmail.com>
2013-01-10 17:34:02 +0000

    search users 1.1

--- vdl-themes/default/search.php
+++ vdl-themes/default/search.php
@@ -1,6 +1,6 @@
-<h1>Ola ke dise</h1>
 <?php
 	//aqui encajar todo lo visual, y comprueben como se envian las cosas
+	$ID = ID;
 	$c_user = new CORE_USER();
 	$found_users = $c_user->search_users($_GET['p2']);
 ?>
@@ -9,10 +9,10 @@
 	<ul>
 		<?php
 			foreach ($found_users as $found){
-				echo '<li>';
+				echo '<li><form id="agregar_amigo" class="navbar-form" method="post" action="/Vidali/vdl-include/add_friend.php">';
 				echo '<img src="'.BASEDIR."/vdl-files/".$found[1].'.jpg" width="60" height="60" >';
 				echo $found[0];
-				echo '</li>';
+				echo '<button type="submit" class="btn btn-link">+ Agregar como amigo</button><input type="hidden" name="usuario" value="'.$ID.'"/><input type="hidden" name="amigo" value="'.$found[0].'"/></form></li>';
 			}
 		?>
 	</ul>
