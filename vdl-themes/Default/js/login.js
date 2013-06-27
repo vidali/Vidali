2e46cda6b39134a7423a81cf44cf0641d06acf53
cristomc <cristo-mc@hotmail.com>
2013-06-27 09:59:18 +0100

    Updating register, enabling offline mode

--- vdl-themes/Default/js/login.js
+++ vdl-themes/Default/js/login.js
@@ -1,9 +1,11 @@
+var msgTimeout;
 var errMsg = function(msg, type){
 	clearTimeout(msgTimeout);
+	$("#alert").empty();
+	$("#alert").append("<strong>Ops!</strong>");
 	$("#alert").fadeOut("fast",function(){
-		$("#alert span").html(msg);
 		$("#alert").attr("class","alert " + (type ? "alert-"+type : "alert-message"));
-		
+		$("#alert").append(" "+msg);
 		$("#alert").fadeIn("fast");
 		msgTimeout = setTimeout(function(){$('#alert').fadeOut();},4000);
 	});
@@ -15,14 +17,14 @@ var doLogin = function(){
     var dir;
 	
 	if(!user || !password){
-		errMsg('Debes rellenar todos los datos.');
+		errMsg('All data must be filled');
 		return;
 	}
 	
 	var a = user.indexOf('@');
 	var b = user.indexOf('.',a);
 	if(a == -1 || a == 0 || b == -1 || a+1 == b || b+1 == user.length){
-		errMsg('El correo introducido es incorrecto.');
+		errMsg('Incorrect Email format');
 		return;
 	}
 	if(basedir.length == 0)
@@ -47,7 +49,55 @@ var doLogin = function(){
 				});
 			} else {
 				$("#background").fadeOut(500, function(){
-					errMsg('Usuario o contrase&ntilde;a incorrectos.',"error");
+					errMsg('Wrong user or password, please try again.',"error");
+				});
+				
+			}
+		}
+	});
+}
+
+var doRegister = function(){
+	var user = $('#r_nick').val();
+	var password = $('#r_pass').val();
+	var email = $('#r_email').val();
+    var dir;
+
+	if(!user || !password || !email){
+		errMsg('You must fill all fields.');
+		return;
+	}
+
+	var a = email.indexOf('@');
+	var b = email.indexOf('.',a);
+	if(a == -1 || a == 0 || b == -1 || a+1 == b || b+1 == email.length){
+		errMsg('Wrong email.');
+		return;
+	}
+	if(basedir.length == 0)
+        dir = "/vdl-include/reg.php";
+    else
+        dir = basedir+"/vdl-include/reg.php"; 
+	
+	$("#background").fadeIn(500);
+	$.ajax({
+		url: dir,
+		cache: true,
+		type: "POST",
+		data: {
+			user: user,
+			password: password,
+			remember: $('#allow_ads').prop('checked') ? 1 : 0
+		},
+		success: function(data){
+			if(data == "1" || data == "true"){
+				$("#background").fadeIn(500, function (){
+					window.location= basedir;
+					errMsg('Wellcome to Onvidali. Please log in.',"sucess");
+				});
+			} else {
+				$("#background").fadeOut(500, function(){
+					errMsg('Register is unavailable at this moment, please try again later.',"error");
 				});
 				
 			}
